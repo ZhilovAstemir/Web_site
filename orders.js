cfb7f0fa9babc330e055c1c2a60d7501fff58e19
Astemir Bekov <99669604+ZhilovAstemir@users.noreply.github.com>
2022-04-01 21:15:55 +0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders = [
+    {
+        productName: 'NFT Dron Mini',
+        productNumber: '56451',
+        paymentStatus: 'Due', 
+        shipping: 'Pending'
+    }, 
+    {
+        productName: 'LERNOR Cross Drone',
+        productNumber: '11542',
+        paymentStatus: 'Paid', 
+        shipping: 'Pending'
+    }, 
+    {
+        productName: 'Drom with Mini Camera',
+        productNumber: '28499',
+        paymentStatus: 'Paid', 
+        shipping: 'Declined'
+    }, 
+    {
+        productName: 'Mini HighTech Drone',
+        productNumber: '54192',
+        paymentStatus: 'Due', 
+        shipping: 'Pending'
+    }, 
+    {
+        productName: 'COLOSS MAP Drone',
+        productNumber: '98415',
+        paymentStatus: 'Paid', 
+        shipping: 'Declined'
+    }, 
+    {
+        productName: 'GPS 5G Drone',
+        productNumber: '98526',
+        paymentStatus: 'Due', 
+        shipping: 'Pending'
+    },
+    {
+        productName: 'REALM F15 Drone',
+        productNumber: '87981',
+        paymentStatus: 'Paid', 
+        shipping: 'Pending'
+    }
+    
+]
